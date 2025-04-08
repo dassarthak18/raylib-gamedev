6a931cb0c4abd65f4600078649b0d5cf02b5036d
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-04-09 00:38:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,15 +34,16 @@ cmake . -DCMAKE_TOOLCHAIN_FILE=../mingw-toolchain.cmake -DCMAKE_INSTALL_PREFIX=.
 cmake . -DCMAKE_TOOLCHAIN_FILE=../mingw-toolchain.cmake -DCMAKE_INSTALL_PREFIX=../bin -DCMAKE_CXX_FLAGS="-includeforce_includes.h"
 make -j$(nproc)
 sudo cmake -DCMAKE_INSTALL_PREFIX=../bin/ -P cmake_install.cmake
+mkdir build-web && cd build-web
 export PKG_CONFIG_PATH=/usr/lib/pkgconfig:/usr/share/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig
-emcmake cmake . -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+emcmake cmake .. -DCMAKE_TOOLCHAIN_FILE=$EMSDK/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
 -DCMAKE_CROSSCOMPILING_EMULATOR=$EMSDK/node/20.18.0_64bit/bin/node \
 -DGLFW_BUILD_WAYLAND=OFF -DGLFW_BUILD_X11=OFF -DGLFW_USE_OSMESA=OFF \
 -DGLFW_BUILD_WIN32=OFF -DGLFW_BUILD_COCOA=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF \
 -DCMAKE_C_FLAGS="-std=gnu99 -pthread -s USE_PTHREADS=1 -DPOSIX_REQUIRED_STANDARD=199309L -D_POSIX_C_SOURCE=POSIX_REQUIRED_STANDARD -D_POSIX_SOURCE=POSIX_REQUIRED_STANDARD" \
--DCMAKE_CXX_FLAGS="-pthread -s USE_PTHREADS=1" -DCMAKE_EXE_LINKER_FLAGS="-pthread -s USE_PTHREADS=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0"
+-DCMAKE_CXX_FLAGS="-std=gnu++17 -Wno-nontrivial-memcall -pthread -s USE_PTHREADS=1" -DCMAKE_EXE_LINKER_FLAGS="-pthread -s USE_PTHREADS=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0"
 emmake make -j
-mv box2d ../bin-web/
+cd .. && mv build-web ../bin-web/box2d
 cd .. && rm -rf box2d
 
 # Install JoltPhysics for 3D Physics
@@ -55,8 +56,7 @@ emcmake cmake ../Build/ && emmake make && cd ..
 mv Build-Web ../bin-web/JoltPhysics/
 cd .. && rm -rf JoltPhysics
 
-# Run tests
+# Build for Linux
 GAME_DIR="$(basename "$(pwd)")"
-emcc -v
 g++ src/main.cpp -o "${GAME_DIR}.sh"
 chmod u+x "${GAME_DIR}.sh" && ./"${GAME_DIR}.sh"
